a2d3010f0593953a375186375dc5a710c7b1db05
Talha Kerpicci <talha.kerpicci@gmail.com>
2023-06-14 17:47:20 +0300

    Added file server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	/*
+		After applying http.StripPrefix("/static", fileServer), the prefix "/static" is stripped from the URL path. The resulting modified URL path becomes "css/styles.css".
+		Now, the modified URL path "css/styles.css" is passed to the fileServer, which serves files from the "./ui/static/" directory.
+		It correctly resolves the file path as "./ui/static/css/styles.css" and serves the corresponding file.
+	*/
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
