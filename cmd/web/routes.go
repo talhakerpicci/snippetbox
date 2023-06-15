cfd7dfb3762fc10142dbe93fb31729237b00aa77
Talha Kerpicci <talha.kerpicci@gmail.com>
2023-06-15 15:15:47 +0300

    Initial Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	/*
+		After applying http.StripPrefix("/static", fileServer), the prefix "/static" is stripped from the URL path. The resulting modified URL path becomes "css/styles.css".
+		Now, the modified URL path "css/styles.css" is passed to the fileServer, which serves files from the "./ui/static/" directory.
+		It correctly resolves the file path as "./ui/static/css/styles.css" and serves the corresponding file.
+	*/
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	return mux
+}
