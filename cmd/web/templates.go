092aebce01767ecfd91dab4a6668fa75f3ab50b6
Talha Kerpicci <talha.kerpicci@gmail.com>
2023-06-20 15:58:32 +0300

    Implement template cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,7 +1,42 @@
 package main
 
-import "github.com/talhakerpicci/snippetbox/internal/models"
+import (
+	"html/template" // New import
+	"path/filepath" // New import
+
+	"github.com/talhakerpicci/snippetbox/internal/models"
+)
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		//ts: template set
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
